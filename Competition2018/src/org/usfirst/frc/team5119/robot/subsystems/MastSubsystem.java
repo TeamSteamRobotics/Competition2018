8315060d394d0830542363856c2cfffc0b2de09f
mmarkle <mmarkle@COMRADE-LAPTOP>
2018-02-03 09:50:46 -0600

    added drive and manual mast

--- Competition2018/src/org/usfirst/frc/team5119/robot/subsystems/MastSubsystem.java
+++ Competition2018/src/org/usfirst/frc/team5119/robot/subsystems/MastSubsystem.java
@@ -8,6 +8,7 @@
 package org.usfirst.frc.team5119.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -18,9 +19,14 @@ public class MastSubsystem extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 	
-	Talon mastMotor = new Talon(00000);
+	Talon mastMotor = new Talon(3);
 	DigitalInput originSwitch = new DigitalInput(00000);
 	DigitalInput topSwitch = new DigitalInput(00000);
+	Encoder encoder = new Encoder(00000, 00000, false);
+	
+	public MastSubsystem() {
+		encoder.setDistancePerPulse(00000);
+	}
 	
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
@@ -37,6 +43,10 @@ public class MastSubsystem extends Subsystem {
 	
 	public boolean isAtTop() {
 		return topSwitch.get();
+	}
+	
+	public double getPosition() {
+		return encoder.getDistance();
 	}
 
 }
